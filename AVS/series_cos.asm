a3fd8bc02922c70c628c9bd02534093d760584ec
Dima Kunin <mr.kunin@gmail.com>
2014-05-23 03:17:35 +0400

    Pretty code.

--- AVS/series_cos.asm
+++ AVS/series_cos.asm
@@ -1,10 +1,11 @@
 ;; compile nasm -felf series_cos.asm && gcc -m32 -o runme series_cos.o
 
+
+;;; <MACRO>
 %macro clear_fpu 0
 	fstp	st0
 %endmacro
 
-	
 %macro fpu_top 0
 	fst	qword [tmp_f]
 	push	dword [tmp_f + 4]
@@ -14,28 +15,37 @@
 	add	esp, 12		; print temp mult
 %endmacro
 
-	
 %macro log_d_var	2
 	
 	section .data
-	str:	db	%1, 0xA, 0
+	.str:	db %1, 0
 	
 	section .text
 	push	dword [%2 + 4]
 	push 	dword [%2]
-	push 	str
+	push 	.str
 	call	printf
 	add	esp, 12		; print temp mult
 	
 %endmacro
-	
-	
+
+%macro new_line 0
+	section .data
+	eol:	db "", 0xA, 0
+
+	section .text
+	push	eol
+	call	printf
+	add	esp, 4
+%endmacro
+
+;;; <EXTERN>
 extern 	printf
+;;; 
 
+;;; <MAIN>
 section .text
-
 global main
-
 main:
 	;; set up x
 	fld	qword [a]
@@ -55,14 +65,7 @@ loop_x:
 	mov	dword [n], 0 	; n = 1
 	;; ! reset vars
 
-	push	dword [x + 4]
-	push	dword [x]
-	push	format_f
-	call	printf
-	add	esp, 12		; print current x
-
-	log_d_var "x=%f", x
-	
+	;; log_d_var "x=%f", x
 loop_n:	
 	;; increment n while is not precise
 	fld	qword [x]
@@ -78,8 +81,7 @@ loop_n:
 	;; inc n
 	add	dword [n], 1
 	
-	;; inc xi
-
+	;; <calc xi>
 	;; top
 	fld1
 	fchs			; -1 st0
@@ -89,7 +91,6 @@ loop_n:
 	fmul
 	fmul
 	fmul			; -1*xi*x*x st0
-	
 	;; bottom
 	fild	dword [n]
 	fld1
@@ -104,41 +105,33 @@ loop_n:
 	fld1
 	fsub			; 2*n - 1
 	fmul			; 2n*(2n-1) st0
-
 	;; div
 	fdiv			; -1*xi*x*x/(2n*(2n-1))
-	fpu_top
 	fstp	qword [xi]
 
+	;; update sum
 	fld	qword [xi]
 	fld	qword [sum]
 	fadd
 	fstp	qword [sum]
 
-	mov	ecx, n
-	push	dword [ecx]
-	push	format_d
-	call	printf
-	add	esp, 8 		; print n
-
-	push	dword [sum + 4]
-	push 	dword [sum]
-	push 	format_f
-	call	printf
-	add	esp, 12		; print sum
-
-	push	dword [xi + 4]
-	push 	dword [xi]
-	push 	format_f
-	call	printf
-	add	esp, 12		; print xi
-
+	;; log_d_var	"sum=%f ", sum
+	;; log_d_var	"xi=%f", xi
+	;; new_line
 	
 	jmp	loop_n
 
 print_row:	
-	;; print result here
-	;; some printf [x, n]
+	;; number of iters = n + 1
+	add	dword [n], 1
+
+	;; print result here	
+	push 	dword[n]
+	push 	dword[x + 4]
+	push 	dword[x]
+	push	format_row
+	call	printf
+	add	esp, 16
 	
 ;;; end loop_n
 
@@ -153,19 +146,22 @@ print_row:
 done:
 	ret
 
+;;; <DATA>
 section .data
 
 format_d:	db		"%d", 0xA, 0
 format_f:	db		"%f", 0xA, 0
 format_fpu:	db		"st0=%f", 0xA, 0
-
+format_row:	db		"[x=%f, n=%d]", 0xA, 0
+	
 ;; For problem
 x:		dq	0.0
-a:		dq	-1.0
-b:		dq	1.0
+a:		dq	-3.15
+b:		dq	3.15
 h:		dq	0.1
 n:		dd	0
-prs:		dq	0.01
+prs:		dq	0.00000001
+;;; 
 
 xi:		dq	0.0
 sum:		dq	0.0
