6ad99df8636fb31589ce8b85f6794681a72ca711
Dima Kunin <mr.kunin@gmail.com>
2014-05-23 01:18:30 +0400

    trying to get it work

--- AVS/series_cos.asm
+++ AVS/series_cos.asm
@@ -1,5 +1,9 @@
 ;; compile nasm -felf series_cos.asm && gcc -m32 -o runme series_cos.o
 
+%macro clear_fpu 0
+	fstp	st0
+%endmacro
+	
 extern 	printf
 
 section .test
@@ -9,62 +13,121 @@ global main
 main:
 	push	str
 	call	printf
-	add		esp, 4
+	add	esp, 4
 
-	mov		ecx, d_d
+	mov	ecx, d_d
 	push 	dword [ecx]
 	push 	format_d
 	call	printf
-	add		esp, 8
+	add	esp, 8
 
 
 	push	dword [d_lf + 4]
 	push 	dword [d_lf]
 	push 	format_f
 	call	printf
-	add		esp, 12
+	add	esp, 12
 
-	fld		dword [d_f]
+	fld	dword [d_f]
 	fstp	qword [some]
 	
 	push	dword [some + 4]
 	push 	dword [some]
 	push 	format_f
 	call	printf
-	add		esp, 12
+	add	esp, 12
 
-loop:
-	fld		qword [d_lf]
-	fld		dword [d_f]
+loop_x:
+	fld	qword [b]
+	fld	qword [x]
 	fcomip
-	fstp 	st0
-	ja	greater
+	clear_fpu
+	ja	done
 
-	push	ls
-	call 	printf
-	add		esp, 4
+	;; reset vars
+	fld1
+	fstp	qword [xi] 	; xi = 1
+	fld1
+	fstp	qword [sum]	; sum = 1
 
-	;; increment
-	fld		dword [d_f]
-	fld		dword [h]
-	fadd
-	fstp	dword[d_f]
+	mov	eax, 1
+	mov	dword [n], eax
+
 	
-	jmp 	loop
+	
+loop_n:	
+	;; increment n while does not precise
+	fld	qword [x]
+	fcos 			; cosX st0
+	fld	qword [sum]
+	fld	qword [xi]
+	fadd			; sum + xi st0
+	fsub			; sum - cosX st0
+	fabs			; abs(sum - cosX) st0
+	fld	qword [prs]	; prs st0
+	fcomip
+	clear_fpu
+	ja	print_row	; if (prs > abs) { print_result }
+	
+	;; inc n
+	mov	eax, n
+	inc	eax
+	mov	dword [n], eax
+	;; inc xi
+	fld	dword [n]
+	fld1
+	fld1
+	fadd
+	fmul			; 2*n
+	fld	dword [n]
+	fld1
+	fld1
+	fadd
+	fmul			; 2*n
+	fld1
+	fsub			; 2*n - 1
+	fmul			; 2n*(2n-1) st0
+
+	fld1
+	fchs			; -1 st0
+	fld	qword [xi]
+	fld	qword [x]
+	fld	qword [x]
+	fmul
+	fmul
+	fmul			; -1*xi*x*x st0
+	fdiv			; -1*xi*x*x/(2n*(2n-1))
+	fstp	qword [xi]
+	
+	jmp	loop_n
+
+print_row:	
+	;; print result here
+	;; some printf [x, n]
+	
+;;; end loop_n
+
+	;; inc x
+	fld	qword [x]
+	fld	qword [h]
+	fadd
+	fstp	qword[x]
+	jmp 	loop_x
+;;; end loop_x
 
 greater:
 	push	gt
 	call	printf
-	add		esp, 4
+	add	esp, 4
 
-	fld		dword [d_f]
+	fld	dword [d_f]
 	fstp	qword [some]
 	
 	push	dword [some + 4]
 	push 	dword [some]
 	push 	format_f
 	call	printf
-	add		esp, 12
+	add	esp, 12
 
 
 done:
@@ -78,8 +141,18 @@ format_f:	db		"%f", 0xA, 0
 d_d:		dd		-4244
 d_lf:		dq		-0.12342
 d_f:		dd		-0.97 ; is less
-h:			dd		0.01
+h:			dd		0.1
 some:		dq		0x1
 
 gt: 	db	"Greater", 0xA, 0
 ls:		db	"Less",	   0xA, 0
+
+;; For problem
+x:		dq	0.0
+a:		dq	-1.0
+b:		dq	1.0
+n:		dd	0
+prs:		dq	0.01
+
+xi:		dq	1
+sum:		dq	0
