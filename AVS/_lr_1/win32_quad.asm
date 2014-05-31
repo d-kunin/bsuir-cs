805708d8ccf4865f1f584ee219508ab41cea1df7
mr.kunin <mr.kunin@gmail.com>
2014-05-31 22:58:37 +0300

    Parse argument from stdin.

--- AVS/_lr_1/win32_quad.asm
+++ AVS/_lr_1/win32_quad.asm
@@ -8,20 +8,18 @@
 ;;; <EXTERN>
 	global task1
 	extern atof
-	extern puts
 	extern printf
 	extern scanf
 ;;; </EXTERN>
 	
 
 ;;; <MACRO>
-;; simple string printing
-%macro 	dprint 1 ; macro with one argument
+%macro 	print 1
 	push 	%1
-	call 	puts
+	call 	printf
 	add		esp, 4
 %endmacro
-;; doulbe print : 1 arg
+
 %macro fprint 2
 	push	dword[%2 + 4]
 	push	dword[%2]
@@ -29,7 +27,7 @@
 	call	printf
 	add		esp, 12
 %endmacro
-;; double print: 3 args
+
 %macro fprint 4
 	push	dword[%4 + 4]
 	push	dword[%4]
@@ -42,30 +40,57 @@
 	add		esp, 28
 %endmacro
 
+;; read double from stdin
+;; args 1 -- dq variable
+%macro parse_double 1
+	push	%1
+	push	format_double
+	call	scanf
+	add		esp, 8
+	cmp		eax, 1
+	jne		parse_error
+%endmacro
+
 ;; read double from string
+;; args 1 -- string, 2 -- dq variable
 %macro parse_double 2
 	push	dword[%1]
 	call	atof
 	add		esp, 4
 	fstp	qword[%2]
 %endmacro
+;;; </MACRO>
 
 ;;; <MAIN>
 section .text
 task1:
 	;; check arguments count
 check_args:
-	mov	eax, dword[esp + 4]
-	cmp	eax, 4
-	jne	error_num_args
+	mov		eax, dword[esp + 4]
+	cmp		eax, 4
+	jne		ask_args
+	jmp		read_args
 
+ask_args:
+	print	msg_ask_args
+	print	a_eq  
+	parse_double	a
+	print	b_eq
+	parse_double	b
+	print	c_eq
+	parse_double	c
+	jmp		do_work
+	
+read_args:
 	;; read arguments from argc/args
 	mov		esi, dword [esp + 8]
 	parse_double	esi+4,  a
 	parse_double 	esi+8, b
 	parse_double	esi+12, c
-	fprint			eq_format, a, b, c
-	
+	jmp				do_work	
+
+do_work:
+	fprint	eq_format, a, b, c
 	;; discriminant
 	fld		qword[c]
 	fmul	qword[a]
@@ -77,7 +102,7 @@ check_args:
 	fadd
 	fmul
 	fmul
-	fld	qword[b]
+	fld		qword[b]
 	fmul	st0, st0	; b^2
 	fsub	st0, st1	; b^2 - 4ac
 	fst		qword[d]
@@ -88,9 +113,9 @@ check_args:
 	ja		negative_d
 
 	;; calc x1
-	fld	qword[d]
+	fld		qword[d]
 	fsqrt
-	fld	qword[b]
+	fld		qword[b]
 	fchs
 	fsub	st0, st1	; -b - sqrt(d)
 	fld1
@@ -100,9 +125,9 @@ check_args:
 	fdiv	qword[a]
 	fstp	qword[x1]
 	;; cals x2
-	fld	qword[d]
+	fld		qword[d]
 	fsqrt
-	fld	qword[b]
+	fld		qword[b]
 	fchs
 	fadd	st0, st1	; -b + sqrt(d)
 	fld1
@@ -110,7 +135,7 @@ check_args:
 	fadd
 	fmul	qword[a]
 	fdiv
-	fstp		qword[x2]
+	fstp	qword[x2]
 
 	;; print result
 	fprint	x1_eq, x1
@@ -118,23 +143,21 @@ check_args:
 
 	jmp	done
 
-error_num_args:
-	dprint	badArgumentsCount
-	jmp	done
-
-negative_d:
-	dprint	msgNegativeD
-	jmp	done
-	
 done:
-	dprint	str_done
+	print	str_done
+	ret
+;; Errors
+negative_d:
+	print	msgNegativeD
+	ret
+	
+parse_error:
+	print msg_dpe
 	ret
 ;;; </MAIN>
 
 ;;; <DATA>
 section .data		
-badArgumentsCount:
-    db      "Requires exactly three arguments", 0xA, 0
 msgNegativeD:
 	db	"Disriminant is less than 0. No solutions in R.", 0xA, 0
 str_done:
@@ -144,6 +167,14 @@ eq_format:	db	"%f*x^2+%f*x+%f = 0", 0xA, 0
 x1_eq:		db	"x1=%f", 0xA, 0
 x2_eq:		db	"x2=%f", 0xA, 0
 d_eq:		db	"D=%f",  0xA, 0
+
+;; arguments prompt
+a_eq:	db	"a=", 0
+b_eq:	db	"b=", 0
+c_eq:	db	"c=", 0
+msg_ask_args:	db	"Enter equation coefficients", 0xA, 0
+format_double: 	db 	"%lf", 0
+msg_dpe: 		db	"Are you sure that was floating point value?", 0xA, 0
 	
 x1:	dq	0.0
 x2:	dq	0.0
