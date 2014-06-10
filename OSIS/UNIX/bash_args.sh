dbfc129d2b5743dc20d64f4aa2ff2eb8f1e0ea4f
Dmitry Kunin <7times6@yandex-team.ru>
2014-06-10 22:25:16 +0300

    history args multiline result as value

---
+++ OSIS/UNIX/bash_args.sh
@@ -0,0 +1,6 @@
+#/bin/bash
+
+echo "Number of args \$#: $#"
+echo "Script name \$0: $0"
+echo "All script params \$*: $*"
+echo "Second script param \$2: $2"
---
+++ OSIS/UNIX/command_res_as_value.sh
@@ -0,0 +1,4 @@
+#/bin/bash
+
+NUMBER_OF_LINES=`cat "$0" | wc -l`
+echo "This script has $NUMBER_OF_LINES lines"
